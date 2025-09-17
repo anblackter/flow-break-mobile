295adb035640e8c83ed6a35294c05176554c3dc4
Angel Henao <ay.henao@uniandes.edu.co>
2025-09-16 21:16:22 -0500

    Basic JetPack Compose Application with Empty Activity

---
+++ app/src/main/java/co/edu/uniandes/miso/ux/flowbreak/MainActivity.kt
@@ -0,0 +1,47 @@
+package co.edu.uniandes.miso.ux.flowbreak
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.uniandes.miso.ux.flowbreak.ui.theme.FlowBreakMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlowBreakMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Flow Break",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FlowBreakMobileTheme {
+        Greeting("Flow Break")
+    }
+}
