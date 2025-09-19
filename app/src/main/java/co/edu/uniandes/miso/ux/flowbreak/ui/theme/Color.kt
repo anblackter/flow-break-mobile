00a80211fc424ec3ea109d65eba48746e6cb7334
Angel Henao <ay.henao@uniandes.edu.co>
2025-09-18 23:25:41 -0500

    feat: Add Color base on Tonal Palettes and customize M3

--- app/src/main/java/co/edu/uniandes/miso/ux/flowbreak/ui/theme/Color.kt
+++ app/src/main/java/co/edu/uniandes/miso/ux/flowbreak/ui/theme/Color.kt
@@ -2,10 +2,70 @@ package co.edu.uniandes.miso.ux.flowbreak.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val FB_Light_Primary0 = Color(0xFF000000)
+val FB_Light_Primary10 = Color(0xFF00000F)
+val FB_Light_Primary20 = Color(0xFF020031)
+val FB_Light_Primary30 = Color(0xFF060057)
+val FB_Light_Primary40 = Color(0xFF0E117C)
+val FB_Light_Primary50 = Color(0xFF1B2E96)
+val FB_Light_Primary60 = Color(0xFF2D47B0)
+val FB_Light_Primary65 = Color(0xFF4260CB)
+val FB_Light_Primary70 = Color(0xFF5779E6)
+val FB_Light_Primary75 = Color(0xFF6F93FF)
+val FB_Light_Primary80 = Color(0xFF8FADFF)
+val FB_Light_Primary85 = Color(0xFFAFC5FF)
+val FB_Light_Primary90 = Color(0xFFCFDDFF)
+val FB_Light_Primary95 = Color(0xFFF0F4FF)
+val FB_Light_Primary100 = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+// Secondary
+val FB_Light_Secondary0 = Color(0xFF000000)
+val FB_Light_Secondary10 = Color(0xFF000C01)
+val FB_Light_Secondary20 = Color(0xFF001F02)
+val FB_Light_Secondary30 = Color(0xFF003405)
+val FB_Light_Secondary40 = Color(0xFF014A0B)
+val FB_Light_Secondary50 = Color(0xFF016312)
+val FB_Light_Secondary60 = Color(0xFF077D1B)
+val FB_Light_Secondary65 = Color(0xFF309637)
+val FB_Light_Secondary70 = Color(0xFF4CAF50)
+val FB_Light_Secondary75 = Color(0xFF66C969)
+val FB_Light_Secondary80 = Color(0xFF81E382)
+val FB_Light_Secondary85 = Color(0xFF9BFE9B)
+val FB_Light_Secondary90 = Color(0xFFCEFFCD)
+val FB_Light_Secondary95 = Color(0xFFF4FFF3)
+val FB_Light_Secondary100 = Color(0xFFFFFFFF)
+
+// Tertiary
+val FB_Light_Tertiary0 = Color(0xFF000000)
+val FB_Light_Tertiary10 = Color(0xFF020000)
+val FB_Light_Tertiary20 = Color(0xFF140200)
+val FB_Light_Tertiary30 = Color(0xFF2F0600)
+val FB_Light_Tertiary40 = Color(0xFF4C0f00)
+val FB_Light_Tertiary50 = Color(0xFF6C1900)
+val FB_Light_Tertiary60 = Color(0xFF8D2400)
+val FB_Light_Tertiary65 = Color(0xFFAE360F)
+val FB_Light_Tertiary70 = Color(0xFFCA502E)
+val FB_Light_Tertiary75 = Color(0xFFE66947)
+val FB_Light_Tertiary80 = Color(0xFFFF8564)
+val FB_Light_Tertiary85 = Color(0xFFFFAD96)
+val FB_Light_Tertiary90 = Color(0xFFFFCBBC)
+val FB_Light_Tertiary95 = Color(0xFFFFE8E1)
+val FB_Light_Tertiary100 = Color(0xFFFFFFFF)
+
+// Light Theme
+val fb_theme_light_primary = FB_Light_Primary40
+val fb_theme_light_onPrimary = FB_Light_Primary100
+val fb_theme_light_primaryContainer = FB_Light_Primary90
+val fb_theme_light_onPrimaryContainer = FB_Light_Primary30
+
+val fb_theme_light_secondary = FB_Light_Secondary40
+val fb_theme_light_onSecondary = FB_Light_Secondary40
+val fb_theme_light_secondaryContainer = FB_Light_Secondary90
+val fb_theme_light_onSecondaryContainer = FB_Light_Secondary30
+
+val fb_theme_light_tertiary = FB_Light_Tertiary80
+val fb_theme_light_onTertiary = FB_Light_Tertiary100
+val fb_theme_light_tertiaryContainer = FB_Light_Tertiary90
+val fb_theme_light_onTertiaryContainer = FB_Light_Tertiary30
--- app/src/main/java/co/edu/uniandes/miso/ux/flowbreak/ui/theme/Theme.kt
+++ app/src/main/java/co/edu/uniandes/miso/ux/flowbreak/ui/theme/Theme.kt
@@ -1,58 +1,42 @@
 package co.edu.uniandes.miso.ux.flowbreak.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = fb_theme_light_primary,
+    onPrimary = fb_theme_light_onPrimary,
+    primaryContainer = fb_theme_light_primaryContainer,
+    onPrimaryContainer = fb_theme_light_onPrimaryContainer,
+    secondary = fb_theme_light_secondary,
+    onSecondary = fb_theme_light_onSecondary,
+    secondaryContainer = fb_theme_light_secondaryContainer,
+    onSecondaryContainer = fb_theme_light_onSecondaryContainer,
+    tertiary = fb_theme_light_tertiary,
+    onTertiary = fb_theme_light_onTertiary,
+    tertiaryContainer = fb_theme_light_tertiaryContainer,
+    onTertiaryContainer = fb_theme_light_onTertiaryContainer
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun FlowBreakMobileTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    val colorScheme = if (darkTheme) {
+            LightColorScheme // TODO: Dark theme not supported yet
+        } else {
+            LightColorScheme
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = shapes,
         content = content
     )
 }
